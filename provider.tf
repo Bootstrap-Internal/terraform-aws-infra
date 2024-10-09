d77f79f4568b5a59807a5e2541087cd3f8ea1360
zeeahmed2 <zeeahmed2@publicisgroupe.net>
2024-10-09 14:58:33 +0530

    updating provider

--- provider.tf
+++ provider.tf
@@ -1,6 +1,3 @@
-# Specify the provider and access details
 provider "aws" {
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
-    region     = var.aws_region
+  region = "us-east-1"  
 }
